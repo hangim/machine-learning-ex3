d810c470b3336d13496e1bca5b1730e223d9e21f
hang <git@hang.im>
2015-11-02 17:12:08 +0800

    One-vs-All Classifier Prediction

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -29,9 +29,9 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
-
+H = sigmoid(X * all_theta');
+[val, index] = max(H, [], 2);
+p = index;
 
 
 
